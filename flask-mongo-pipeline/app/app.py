2d0839dc7146f4875ddfb54cc549722493ff4702
juan <juanhermida221513@gmail.com>
2026-05-02 15:28:48 -0400

    udate to flask 3.0 and bugfix

--- flask-mongo-pipeline/app/app.py
+++ flask-mongo-pipeline/app/app.py
@@ -1,22 +1,11 @@
 from flask import Flask, jsonify
-from pymongo import MongoClient
-import os
+import socket
 
 app = Flask(__name__)
 
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb://admin:ChangeMe123@mongo:27017/?authSource=admin")
-client = MongoClient(MONGO_URI)
-db = client["flask_db"]
-
 @app.route("/")
 def home():
-    db.stats.update_one(
-        {"_id": "hits"},
-        {"$inc": {"count": 1}},
-        upsert=True
-    )
-    count = db.stats.find_one({"_id": "hits"})["count"]
-    return f"<h1>Flask + MongoDB Pipeline</h1><p>Page hits: {count}</p>"
+    return f"<h1>USF Flask App</h1><p>Running on host: {socket.gethostname()}</p>"
 
 @app.route("/health")
 def health():
